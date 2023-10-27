a213a64e9f04f6c04fb70a926ce9c0c97d778b52
iaifilmsbotz <149155432+iaifilmsbotz@users.noreply.github.com>
2023-10-27 20:50:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -855,7 +855,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('⤬ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⤬', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
                   ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
