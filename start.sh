dba6168786880361084aa6a97bd9ae4efd2c1703
RAM <149155432+iaifilmsbotz@users.noreply.github.com>
2024-05-10 10:32:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iaifilmsbotz/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/iaifilmsbotz/WiseCinema.git /WiseCinema
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /WiseCinema
 fi
-cd /PiroAutoFilterBot
+cd /WiseCinema
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting WiseCinema...."
 python3 bot.py
